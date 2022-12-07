89a912e35e2a57b888482b802b69b19a48dc00b0
Edu Ferreira <edubf22@gmail.com>
2022-12-07 13:04:53 -0500

    Fixed typo in subheader

--- src/app.py
+++ src/app.py
@@ -42,7 +42,7 @@ def main():
 
     # Page title
     st.title('NBA Game Outcome Prediction')
-    st.st.subheader("By Eduardo (Edu) Ferreira")
+    st.subheader("By [Eduardo (Edu) Ferreira](https://github.com/edubf22)")
     st.write('Enter the stats for the visitor and home teams and get a prediction of who will win the game!')
     st.write('At the current stage, the stats need to be entered manually. You can find the stats in the folloing page: [Basketball-Reference](https://www.basketball-reference.com/leagues/NBA_2023.html)')
     st.set_page_config(page_title="NBA Predictions", page_icon=":basketball:", layout="centered")
